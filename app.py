d850afbe756b161578e0aa603efd78770a7f6930
Kanchipati Varshith <kanchipati@jmangroup.com>
2025-05-22 18:47:46 +0530

    changed localhost

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def home():
     return render_template('index.html', result=False)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
